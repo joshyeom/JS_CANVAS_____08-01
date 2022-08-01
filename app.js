ecc122f8c73f555c7c8fa5008adc46e1b2d41e9f
joshyeom <105287510+joshyeom@users.noreply.github.com>
2022-08-01 15:09:09 +0900

    0801__Canvas__moveTo,LineTo,Stroke

--- app.js
+++ app.js
@@ -3,15 +3,9 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
 
-ctx.fillRect(50, 50, 100, 200); //margin 50, width 100 height 200인 rect를 fill하는 변수
-
-ctx.rect(150, 150, 100, 100); //rect 생성
-ctx.rect(250, 250, 100, 100);
-ctx.rect(350, 350, 100, 100);
-ctx.fill(); //위 rect들은 같은 path이기 때문에 새로 path를 지정해줘야 함.
-
-ctx.beginPath();
-ctx.rect(450, 450, 100, 100);
-ctx.rect(550, 550, 100, 100);
-ctx.fillStyle = "red";
-ctx.fill(); //위 두개의 rect만 red
+ctx.moveTo(50, 50); //Default값은 0,0 canvas의 맨왼쪽위에서 시작
+ctx.lineTo(150, 50); //붓으로 쭉 그린다고 생각하면 됨. x축으로 150이동
+ctx.lineTo(150, 150);
+ctx.lineTo(50, 150);
+ctx.lineTo(50, 50);
+ctx.fill(); //선으로 그린
