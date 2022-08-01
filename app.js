87bf2b5acd1c70e4d627589883d7e87ba18d5289
joshyeom <105287510+joshyeom@users.noreply.github.com>
2022-08-01 17:16:53 +0900

    08/01__Canvas__Drawing 2

--- app.js
+++ app.js
@@ -2,28 +2,28 @@ const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
+ctx.lineWidth = 2;
 
-const colors = [
-  "#c56cf0",
-  "#ffb8b8",
-  "#ff3838",
-  "#ff9f1a",
-  "#ff9f1a",
-  "#fff200",
-  "#32ff7e",
-  "#7efff5",
-  "#18dcff",
-  "#7d5fff",
-  "#4b4b4b",
-];
+let isPainting = false; //let으로 startPainting에서 true로 재할당
 
-function mouseMove(event) {
-  ctx.beginPath(); //mousemove 때마다 beginPath를 해줌으로써 같은 path가 아니게끔 하여 새로운색으로 색칠가능.
-  ctx.moveTo(0, 0);
-  const color = colors[Math.floor(Math.random() * colors.length)];
-  ctx.strokeStyle = color;
-  ctx.lineTo(event.offsetX, event.offsetY); //마우스 좌표를 나타내주는 offsetX,Y
-  ctx.stroke();
+function onMove(event) {
+  if (isPainting) {
+    ctx.lineTo(event.offsetX, eventoffsetY);
+    ctx.fill();
+    return;
+  }
+  ctx.moveTo(event.offsetX, event.offsetY);
 }
 
-canvas.addEventListener("mousemove", mouseMove);
+function startPainting() {
+  isPainting = ture;
+}
+
+function cancelPainting() {
+  isPainting = false;
+}
+
+canvas.addEventListener("mousemove", onMove);
+canvas.addEventListener("mousedown", startPainting); //mouse를 click에서 누르고 있을 때
+canvas.addEventListener("mouseup", cancelPainting); //mouse를 click에서 뗀 순간
+canvas.addEventListener("mouseleave", cancelPainting); //마우스가 canvas를 벗어나고 그려지는것는 버그 방지
