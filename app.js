bae3044a9a39b5d75f5d79f1a80f463aad9cbcbf
joshyeom <105287510+joshyeom@users.noreply.github.com>
2022-08-01 16:24:03 +0900

    08/01__Canvas__Drawing

--- app.js
+++ app.js
@@ -3,15 +3,27 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
 
-ctx.fillRect(210 - 40, 200 - 20, 15, 100);
-ctx.fillRect(350 - 40, 200 - 20, 15, 100);
-ctx.fillRect(260 - 40, 200 - 20, 60, 200);
+const colors = [
+  "#c56cf0",
+  "#ffb8b8",
+  "#ff3838",
+  "#ff9f1a",
+  "#ff9f1a",
+  "#fff200",
+  "#32ff7e",
+  "#7efff5",
+  "#18dcff",
+  "#7d5fff",
+  "#4b4b4b",
+];
 
-ctx.arc(250, 100, 50, 0, 2 * Math.PI); //2 * Math.PI = circle
-ctx.fill();
+function mouseMove(event) {
+  ctx.beginPath(); //mousemove 때마다 beginPath를 해줌으로써 같은 path가 아니게끔 하여 새로운색으로 색칠가능.
+  ctx.moveTo(0, 0);
+  const color = colors[Math.floor(Math.random() * colors.length)];
+  ctx.strokeStyle = color;
+  ctx.lineTo(event.offsetX, event.offsetY); //마우스 좌표를 나타내주는 offsetX,Y
+  ctx.stroke();
+}
 
-ctx.beginPath(); //없이 진행하면 위에 색상과 겹쳐져서 이상해짐.
-ctx.fillStyle = "white";
-ctx.arc(260 + 10, 80, 8, Math.PI, 0);
-ctx.arc(220 + 10, 80, 8, Math.PI, 0);
-ctx.fill();
+canvas.addEventListener("mousemove", mouseMove);
