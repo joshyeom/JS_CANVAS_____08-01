b57270e5faece1c866f47024cb237aaabc3f0cd6
joshyeom <105287510+joshyeom@users.noreply.github.com>
2022-08-02 11:00:37 +0900

    08/02__Canvas__set-rage

--- app.js
+++ app.js
@@ -1,29 +1,38 @@
+const color = document.getElementById("color");
+const lineWidth = document.getElementById("line-width");
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
-ctx.lineWidth = 2;
-
-let isPainting = false; //let으로 startPainting에서 true로 재할당
+ctx.lineWidth = lineWidth.value;
+let isPainting = false;
 
 function onMove(event) {
   if (isPainting) {
-    ctx.lineTo(event.offsetX, eventoffsetY);
-    ctx.fill();
+    ctx.lineTo(event.offsetX, event.offsetY);
+    ctx.stroke();
     return;
   }
+  ctx.beginPath();
   ctx.moveTo(event.offsetX, event.offsetY);
 }
-
 function startPainting() {
-  isPainting = ture;
+  isPainting = true;
 }
-
 function cancelPainting() {
   isPainting = false;
 }
+function onLineWidthChange(event) {
+  ctx.lineWidth = event.target.value;
+}
+function onColorChange(event) {
+  ctx.strokeStyle = event.target.value;
+  ctx.fillStyle = event.target.value;
+}
 
 canvas.addEventListener("mousemove", onMove);
-canvas.addEventListener("mousedown", startPainting); //mouse를 click에서 누르고 있을 때
-canvas.addEventListener("mouseup", cancelPainting); //mouse를 click에서 뗀 순간
-canvas.addEventListener("mouseleave", cancelPainting); //마우스가 canvas를 벗어나고 그려지는것는 버그 방지
+canvas.addEventListener("mousedown", startPainting);
+canvas.addEventListener("mouseup", cancelPainting);
+
+lineWidth.addEventListener("change", onLineWidthChange); //change >> range의 값을 감지
+color.addEventListener("change", onColorChange);
