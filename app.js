81da9dc57119ac342712dbac3032e54f524cfb3d
joshyeom <105287510+joshyeom@users.noreply.github.com>
2022-08-02 14:00:18 +0900

    08/02__Canvas__Array.from, data-,change

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+const colorOptions = Array.from(
+  //유사배열을 배열로 만들어줌
+  //color-option에 있는것들은 배열이 아닌 유사배열(HTMLCollection)이므로, 배열로 만든 후, for.each로 데이터 추출.
+  document.getElementsByClassName("color-option")
+);
 const color = document.getElementById("color");
 const lineWidth = document.getElementById("line-width");
 const canvas = document.querySelector("canvas");
@@ -30,9 +35,18 @@ function onColorChange(event) {
   ctx.fillStyle = event.target.value;
 }
 
+function onColorClick(event) {
+  const colorValue = event.target.dataset.color; //input의 data를 가져옴
+  ctx.strokeStyle = colorValue;
+  ctx.fillStyle = colorValue;
+  color.value = colorValue;
+}
+
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", startPainting);
 canvas.addEventListener("mouseup", cancelPainting);
 
 lineWidth.addEventListener("change", onLineWidthChange); //change >> range의 값을 감지
 color.addEventListener("change", onColorChange);
+
+colorOptions.forEach((color) => color.addEventListener("click", onColorClick));
